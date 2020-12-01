d85b81cd25fbfc96251574b5edb7d5de96ad7f27
superdupermadboy <markusszar@gmail.com>
2020-12-01 19:34:39 +0100

    Add first build

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(eot|woff|woff2|svg|ttf|otf)([\?]?.*)$/,
+        use: ['file-loader']
+      }
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+  }
 };
